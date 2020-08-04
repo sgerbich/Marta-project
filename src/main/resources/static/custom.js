4a3434c77dd5e6467a6a0cf72035bd7ec76b1a80
unknown <sdgerbich@gmail.com>
2020-08-04 16:18:25 -0400

    pop ups kinda working>

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -7,9 +7,29 @@ function initMap() {
     });
 	
     for (i=0; i<busLocations.length; i++){
+        const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Bus Location</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Buslocations</b>"+
+    "</div>" +
+    "</div>";
+        const infowindow = new google.maps.InfoWindow({
+            content: contentString
+          });
         var marker = new google.maps.Marker({
             position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
             map: map,
+        
         });
+        marker.addListener("click", () => {
+            infowindow.open(map, marker);
+          });
     }
 }
+
+
+  
+ 
